a5aab75cf9a9b9bf704a5ae326854d4cc1af80fe
IsaAliev <isaaliev12@gmail.com>
2018-02-19 22:51:13 +0300

    Initial Commit

---
+++ FlexibleNetworkLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlexibleNetworkLayer
+//
+//  Created by Isa Aliev on 19.02.18.
+//  Copyright © 2018 IA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
